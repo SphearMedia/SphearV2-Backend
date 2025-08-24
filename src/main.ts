79162fd31e89f534f7e27daa78b8808144bf8987
milimeter <razaqolad300@gmail.com>
2025-08-24 23:59:19 +0100

    Add global exception filters for improved error handling

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './exceptions/global-exception.filter';
+import { HttpExceptionFilter } from './exceptions/http-exception.filter';
+import { MongoExceptionFilter } from './exceptions/mongo-exception.filter';
+import { MongooseValidationFilter } from './exceptions/mongoose-validation.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(
+    new GlobalExceptionFilter(),
+    new HttpExceptionFilter(),
+    new MongoExceptionFilter(),
+    new MongooseValidationFilter(),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

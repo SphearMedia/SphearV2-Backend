034a43062f830bd85f6935bc97d1195af5fb61ec
milimeter <razaqolad300@gmail.com>
2025-08-24 23:51:58 +0100

    Refactor AppModule to include UsersModule and AuthModule, removing unused AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, AuthModule],
+  controllers: [],
+  providers: [UsersService],
 })
 export class AppModule {}

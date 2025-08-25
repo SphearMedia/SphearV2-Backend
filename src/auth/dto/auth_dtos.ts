03ca6d21850b2e5b9615d77df8a03823ea1ca6b0
milimeter <razaqolad300@gmail.com>
2025-08-25 03:19:57 +0100

    Add DTO classes for email verification, account creation, and user management

---
+++ src/auth/dto/auth_dtos.ts
@@ -0,0 +1,38 @@
+export class RequestEmailVerificationAuthDto {
+  email: string;
+}
+
+export class VerifyEmailAuthDto {
+  email: string;
+  code: string;
+}
+
+export class CreateAccountAuthDto {
+  email: string;
+  password: string;
+  //deviceToken?: string;
+}
+
+export class LoginAuthDto {
+  email: string;
+  password: string;
+  //deviceToken?: string;
+}
+
+export class ResendEmailAuthDto {
+  email: string;
+}
+
+export class ForgotPasswordAuthDto {
+  email: string;
+}
+
+export class ResetPasswordAuthDto {
+  email: string;
+  newPassword: string;
+  code: string;
+}
+
+export class UpdateUserTypeAuthDto {
+  isArtist: boolean;
+}

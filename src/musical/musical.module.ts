882b9aad252316608d095bb93e43fe1b3679543f
milimeter <razaqolad300@gmail.com>
2025-08-30 03:08:12 +0100

    Refactor MusicalModule to include Mongoose integration for Track and Project schemas

--- src/musical/musical.module.ts
+++ src/musical/musical.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MusicalService } from './musical.service';
 import { MusicalController } from './musical.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Project, ProjectSchema } from 'src/models/project.schema';
+import { Track, TrackSchema } from 'src/models/track.schema';
+import { UploaderModule } from 'src/uploader/uploader.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Track.name, schema: TrackSchema },
+      { name: Project.name, schema: ProjectSchema },
+    ]),
+    UsersModule,
+    UploaderModule,
+  ],
   controllers: [MusicalController],
   providers: [MusicalService],
 })

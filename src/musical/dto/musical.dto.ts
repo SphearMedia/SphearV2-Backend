cf1d294d638dcbb088593f0cf4921bc49aa724e8
milimeter <razaqolad300@gmail.com>
2025-08-30 03:08:02 +0100

    Refactor CreateSingleDto and CreateProjectDto for improved validation and structure

--- src/musical/dto/musical.dto.ts
+++ src/musical/dto/musical.dto.ts
@@ -1,3 +1,41 @@
-import { PartialType } from '@nestjs/mapped-types';
+import {
+  IsString,
+  IsOptional,
+  IsBoolean,
+  IsEnum,
+  IsUrl,
+  IsArray,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { GenreEnum } from 'src/enums/track.data.enums';
+export class CreateSingleDto {
+  @IsString() title: string;
+  @IsString() primaryArtist: string;
+  @IsOptional() @IsString() featuredArtists?: string;
+  @IsBoolean() isJointRelease: boolean;
+  @IsEnum(GenreEnum) genre: GenreEnum;
+  @IsUrl() coverArt: string;
+  @IsOptional() @IsString() recordLabel?: string;
+  @IsOptional() @IsString() composer?: string;
+  @IsOptional() @IsString() songWriter?: string;
+  @IsOptional() @IsString() producer?: string;
+  @IsString() trackUrl: string;
+  @IsOptional() @IsString() lyrics?: string;
+}
 
-export class UpdateMusicalDto  {}
+export class CreateProjectDto {
+  @IsString() title: string;
+  @IsString() primaryArtist: string;
+  @IsOptional() @IsString() featuredArtists?: string;
+  @IsBoolean() isJointRelease: boolean;
+  @IsEnum(GenreEnum) genre: GenreEnum;
+  @IsUrl() coverArtUrl: string;
+  @IsOptional() @IsString() recordLabel?: string;
+  @IsString() type: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateSingleDto)
+  tracks: CreateSingleDto[];
+}

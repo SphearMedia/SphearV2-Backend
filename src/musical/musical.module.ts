6fee9707615393ac4b67578d4aa72f6483b10435
milimeter <razaqolad300@gmail.com>
2025-08-31 11:22:38 +0100

    Add PlayHistory schema to MusicalModule imports

--- src/musical/musical.module.ts
+++ src/musical/musical.module.ts
@@ -6,12 +6,14 @@ import { Project, ProjectSchema } from 'src/models/project.schema';
 import { Track, TrackSchema } from 'src/models/track.schema';
 import { UploaderModule } from 'src/uploader/uploader.module';
 import { UsersModule } from 'src/users/users.module';
+import { PlayHistory, PlayHistorySchema } from 'src/models/play.history.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Track.name, schema: TrackSchema },
       { name: Project.name, schema: ProjectSchema },
+      { name: PlayHistory.name, schema: PlayHistorySchema },
     ]),
     UsersModule,
     UploaderModule,

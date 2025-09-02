7be4c3f1d6d50abeb3370a8cbe1c8e0dacadf46f
milimeter <razaqolad300@gmail.com>
2025-09-02 17:39:31 +0100

    Add imports for Mongoose and HttpModule in NotificationsModule

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { NotificationSchema } from 'src/models/notification.schema';
+import { HttpModule } from '@nestjs/axios';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [
+    HttpModule,
+    MongooseModule.forFeature([
+      { name: Notification.name, schema: NotificationSchema },
+    ]),
+    UsersModule,
+  ],
   controllers: [NotificationsController],
   providers: [NotificationsService],
+  exports: [NotificationsService],
 })
 export class NotificationsModule {}

0526675ebbc80ca8ccc8b1cc37216d157c30216d
milimeter <razaqolad300@gmail.com>
2025-09-02 17:37:44 +0100

    Add NotificationType enum to define various notification types

---
+++ src/enums/notification.enum.ts
@@ -0,0 +1,9 @@
+export enum NotificationType {
+  LOGIN = 'login',
+  REGISTRATION = 'registration',
+  PROFILE_UPDATE = 'profile_update',
+  TRANSACTION = 'transaction',
+  REFERRAL = 'referral',
+  ADMIN_ACTION = 'admin_action',
+  SYSTEM = 'system',
+}

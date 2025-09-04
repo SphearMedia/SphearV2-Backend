74479113591c4a20bf3bc136b65d999bd3b4355c
milimeter <razaqolad300@gmail.com>
2025-09-04 15:28:35 +0100

    Import Notification model alongside NotificationSchema in NotificationsModule

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { NotificationSchema } from 'src/models/notification.schema';
+import { NotificationSchema, Notification } from 'src/models/notification.schema';
 import { HttpModule } from '@nestjs/axios';
 import { UsersModule } from 'src/users/users.module';
 

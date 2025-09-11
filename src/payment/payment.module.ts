305da002c4e7ccee01084187d9d00865274c9b89
milimeter <razaqolad300@gmail.com>
2025-09-11 14:41:02 +0100

    Add UsersModule and NotificationsModule to PaymentModule imports

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
 import { HttpModule } from '@nestjs/axios';
+import { NotificationsModule } from 'src/notifications/notifications.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, UsersModule, NotificationsModule],
   controllers: [PaymentController],
   providers: [PaymentService],
 })
